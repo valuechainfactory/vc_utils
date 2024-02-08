4c980fffd46adb4fd31a3b21e027e9db71e9fb9b
Gitahi Erickson <ericksongitahi@gmail.com>
2024-02-08 21:08:07 +0300

    add opts for the request callback

--- lib/vc_utils/http_client/finch.ex
+++ lib/vc_utils/http_client/finch.ex
@@ -2,9 +2,9 @@ defmodule VCUtils.HTTPClient.Finch do
   @behaviour VCUtils.HTTPClient
 
   @impl VCUtils.HTTPClient
-  def request(method, url, headers \\ [], body \\ nil) do
+  def request(method, url, headers \\ [], body \\ nil, opts \\ []) do
     method
-    |> Finch.build(url, headers, body)
+    |> Finch.build(url, headers, body, opts)
     |> Finch.request(VCUtils.Finch)
   end
 end
